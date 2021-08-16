e7b56b001dbfe8e48ccb79df6ea28c7703a7ec9a
wangyx <378739901@qq.com>
2021-08-16 17:41:33 +0800

    debug

--- test_bufio/main.go
+++ test_bufio/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 )
 
@@ -21,4 +23,42 @@ func main() {
 	buf = bufio.NewReader(r)
 	bytesinf, _ = buf.Peek(5)
 	fmt.Println(string(bytesinf))
+
+	strToFile()
+	strFile()
+}
+
+func strToFile() {
+	str := "test hello "
+	ioR := strings.NewReader(str)
+	var fi *os.File
+	var err error
+	fileName := "test_bufio/test.txt"
+	if fi, err = os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0644); err == nil {
+	} else {
+		fmt.Println(" open ")
+		fi, _ = os.Create(fileName)
+	}
+
+	buf := bufio.NewReader(ioR)
+	buf.WriteTo(fi)
+
+	fi.Close()
+}
+
+func strFile() {
+	str := "test hello "
+	ioR := strings.NewReader(str)
+	var fi *os.File
+	var err error
+	fileName := "test_bufio/test1.txt"
+	if fi, err = os.OpenFile(fileName, os.O_RDWR|os.O_APPEND, 0644); err == nil {
+	} else {
+		fmt.Println(" open ")
+		fi, _ = os.Create(fileName)
+	}
+
+	io.Copy(fi, ioR)
+
+	fi.Close()
 }
