a59ff08d80eb6dcad0a309146ce21ba268a9bce2
wangyx <378739901@qq.com>
2021-10-26 21:04:17 +0800

    debug

--- test_parse_url/main.go
+++ test_parse_url/main.go
@@ -7,8 +7,17 @@ import (
 
 func main() {
 	str := "cdn://wss.wangxiao.eaydu.com/live_ali/x_3_test?key=d0e5159c95e4bd63c93ad467bb14f134&tm=74841c9c"
+	//str = "cdn://wss.wangxiao.eaydu.com/live_ali/x_3_test"
 	detail, _ := url.Parse(str)
 	fmt.Println(detail.Host)
 	fmt.Println(detail.Path)
 	fmt.Println(detail.RawQuery)
+
+	query, err := url.ParseQuery(detail.RawQuery)
+	if err != nil {
+		return
+	}
+	for keys, values := range query {
+		fmt.Println(keys, values[0],"||")
+	}
 }
--- test_time/main.go
+++ test_time/main.go
@@ -18,10 +18,25 @@ func main() {
 
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 
+	tt, err := time.Parse(time.RFC3339, "2021-10-26T19:00:55+08:00")
+	if err != nil {
+		return
+	}
+	fmt.Println(tt.Unix(), "rfc3339", time.Now().Unix())
+
 	cun, err := time.Parse("2006-01-02 15:04:05", "2022-02-01 00:00:01")
 
 	hours := cun.Sub(time.Now()).Hours()
 
 	fmt.Println(math.Floor(hours/24), " day ", int(hours)%24, " hours")
 
+	parse, err = time.Parse("2006-01-02 15:04:05", "2021-10-26 18:12:56")
+	if err != nil {
+		return
+	}
+	if parse.Unix()+86400 < time.Now().Unix() {
+		fmt.Println("guo qi ")
+	} else {
+		fmt.Println("0000000 ")
+	}
 }
