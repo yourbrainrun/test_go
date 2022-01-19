ed5785e80093ea3398e902dafe19d87999a47eca
wangyx <378739901@qq.com>
2022-01-19 17:19:29 +0800

    debug

--- test_gin_restful/main.go
+++ test_gin_restful/main.go
@@ -5,22 +5,39 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	v1 := router.Group("/api/vi/user")
+	v1 := router.Group("/v1")
 	{
-		v1.POST("/", Create)
-		v1.POST("/:id", Show)
+		v1.GET("/", Create)
+		v1.GET("/:id", Show)
 	}
+
+	v2 := router.Group("v2")
+	{
+		v2.GET("/", func(c *gin.Context) {
+			c.JSON(200, map[string]string{
+				"v2": "/",
+			})
+		})
+
+		v2.GET("/test", func(c *gin.Context) {
+			c.JSON(200, map[string]string{
+				"v2": "/test",
+			})
+		})
+	}
+
+	router.Run(":8088")
 }
 
 func Create(c *gin.Context) {
 	c.JSON(200, map[string]string{
-		"test": "test",
+		"v1": "test",
 	})
 }
 func Show(c *gin.Context) {
 	id := c.Param("id")
 
 	c.JSON(200, map[string]string{
-		"test": id,
+		"v1": id,
 	})
 }
