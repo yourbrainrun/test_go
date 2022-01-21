8ef54504544b6251ed78176aa8dd445a466eece9
wangyx <378739901@qq.com>
2022-01-21 18:34:34 +0800

    debug

--- test_map/main.go
+++ test_map/main.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
+	type Info struct {
+		Name string
+		Age  int
+		Desc string
+	}
+	var info Info
+	info.Desc = "test"
+	info.Age = 20
+	info.Name = "xx"
+	test(info)
+}
+
+func test(info interface{}) {
+	bytesData, _ := json.Marshal(info)
+	mapData := make(map[string]interface{}, 0)
+	mapData2 := make(map[string]interface{}, 0)
+	_ = json.Unmarshal(bytesData, &mapData)
+
+	for key, value := range mapData {
+		mapData2[key] = value
+	}
+	mapData2["other"] = 123
+
+	bytesData2, _ := json.Marshal(mapData2)
+	fmt.Println(string(bytesData2))
+}
+
+func test1() {
 	maps := make(map[string]string, 0)
 	maps["11"] = "123"
 
