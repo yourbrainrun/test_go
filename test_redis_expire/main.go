94924f852124e5221c3c6c5afb6a9edfbcc2b21e
wangyx <378739901@qq.com>
2022-06-25 16:06:48 +0800

    psubscribe

--- test_redis_expire/main.go
+++ test_redis_expire/main.go
@@ -63,8 +63,41 @@ func main() {
 	var psub PSubscriber
 	psub.PConnect("127.0.0.1", 6397)
 	// 修改为：notify-keyspace-events “Ex”
-	psub.Psubscribe("__keyevent@0__:expired", timeoutCallback)
+	psub.Psubscribe("__keyspace@0__:*", timeoutCallback)
 	for {
 		time.Sleep(1 * time.Second)
 	}
 }
+
+/*
+func Init() {
+	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
+	if err != nil {
+		log.Critical("redis dial failed.")
+	}
+
+	rdb := redis.PubSubConn{conn}
+	if err != nil {
+		log.Critical("redis dial failed.")
+	}
+	ctx := context.Background()
+	go func() {
+		redisSub := rdb.PSubscribe(ctx, "__keyspace@0__:retry_task_keys:*")
+		_, err := redisSub.Receive(ctx)
+		if err != nil {
+			_ = level.Error(helpers.GetLogger()).Log(
+				"class", "RetryTaskWatcher",
+				"method", "Init",
+				"message", err.Error(),
+			)
+			return
+		}
+		ch := redisSub.Channel()
+		for msg := range ch {
+			if msg.Payload == "expired" {
+				log.Critical("expired")
+			}
+		}
+	}()
+}
+*/
